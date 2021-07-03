e0d1ef5c4f965a9a1e718ea3c33f4d281487da60
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-07-03 17:27:02 +0300

    Task list sorted by priority

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,9 +3,33 @@ import { observer } from "mobx-react";
 import taskStore from "../stores/taskStore";
 import TaskItem from "./TaskItem";
 
+const pNum = (task) => {
+  if (task.priority === "low") {
+    return 1;
+  } else if (task.priority === "medium") {
+    return 2;
+  } else if (task.priority === "high") {
+    return 3;
+  } else {
+    console.error("unknown priority", task.priority);
+    return 4;
+  }
+};
+
+const prioritySort = (a, b) => {
+  if (pNum(a) < pNum(b)) {
+    return -1;
+  }
+  if (pNum(a) > pNum(b)) {
+    return 1;
+  }
+  return 0;
+};
+
 const TaskList = () => {
   const taskList = taskStore.tasks
     .filter((task) => task.status === false)
+    .sort(prioritySort)
     .map((task) => <TaskItem task={task} key={task.id} />);
 
   const doneTaskList = taskStore.tasks
